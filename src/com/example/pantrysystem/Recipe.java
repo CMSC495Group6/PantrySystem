4be1e975509c24b6f7924c2bb1326b0d3fb32b3f
CMSC495Group6 <umuc.cmsc495.g6@gmail.com>
2014-12-08 19:15:18 -0500

    Update Recipe.java
    updated item references, removed a function

--- src/com/example/pantrysystem/Recipe.java
+++ src/com/example/pantrysystem/Recipe.java
@@ -7,6 +7,16 @@
  *  and the name of the recipe. It is designed to be implemented 
  *  with the RecipeHandler class, which will contain all the recipes
  *  and handle adding or removing recipes from the application.
+ *  **
+ *  Revisions: 
+ *  *
+ *  12/07/2014
+ *  Converted item types from FullItem to IngredientItem, due to 
+ *  expiration date not being required within a recipes ingredient list.
+ *  Removed notifyUser function
+ *  @author Jesse
+ *  *
+ *  
  */
 package com.example.pantrysystem;
 
@@ -17,21 +27,25 @@ public class Recipe {
 	private String name;
 	
 	// list of items needed to make recipe
-	//TODO extend item class into a RecipeItem class?
-	private ArrayList<FullItem> ingredients;
+	private ArrayList<IngredientItem> ingredients;
 	
 	// Constructors
 	public Recipe() {
-		this.ingredients = new ArrayList<FullItem>();
+		this.ingredients = new ArrayList<IngredientItem>();
 	}	
 	
-	public Recipe(ArrayList<FullItem> ingredients) {
-		this.ingredients = new ArrayList<FullItem>(ingredients);
+	public Recipe(ArrayList<IngredientItem> ingredients) {
+		this.ingredients = new ArrayList<IngredientItem>(ingredients);
 	}
 	
 	public Recipe(Recipe r) {
-		this.ingredients = new ArrayList<FullItem>(r.getList());
+		this.ingredients = new ArrayList<IngredientItem>(r.getList());
 		this.name = r.getName();
+	}
+	
+	public Recipe(String s) {
+		this.ingredients = new ArrayList<IngredientItem>();
+		this.name = s;
 	}
 	
 	// Getters 
@@ -39,7 +53,7 @@ public class Recipe {
 		return this.name;
 	}
 	
-	public ArrayList<FullItem> getList() {
+	public ArrayList<IngredientItem> getList() {
 		return this.ingredients;
 	}
 	
@@ -48,36 +62,25 @@ public class Recipe {
 		this.name = s;
 	}
 	
-	public void addIngredient(FullItem i) {
+	public void addIngredient(IngredientItem i) {
 		// check if recipe already contains item
 		if (this.ingredients.contains(i))
 		{
-			notifyUser(1);
 			//TODO add to quantity or prompt user to redo?
 		}
 		else
 			this.ingredients.add(i);
 	}
 
-	public void setIngredients(ArrayList<FullItem> newList) {
-		this.ingredients = new ArrayList<FullItem>(newList);
-	}
-	
-	/* this function is used to handle unexpected events
-	 * based on the error type.
-	 */
-	void notifyUser(int err) {
-		if (err == 1)
-			System.out.println("Recipe already contained ingredient");
-		else if (err == 2)
-			System.out.println("Recipe does not contain ingredient");
+	public void setIngredients(ArrayList<IngredientItem> newList) {
+		this.ingredients = new ArrayList<IngredientItem>(newList);
 	}
 	
 	private int getSize() {
 		return this.ingredients.size();
 	}
 	
-	private int getIngredientIndex(FullItem i) {
+	private int getIngredientIndex(IngredientItem i) {
 		int size = this.getSize();
 		
 		// loop through the ingredient list and find item with same name
@@ -86,46 +89,39 @@ public class Recipe {
 				return index;
 		
 		// if not found return an invalid index
-		notifyUser(2);
 		return -1;
 	}
 
 	/* updates a specified ingredient quantity to a new set amount
 	 * Does not test for valid quantity
 	 */
-	public void updateQuantity(FullItem i) {
+	public void updateQuantity(IngredientItem i) {
 		int target = this.getIngredientIndex(i);
 
 		// check if the item is found
 		if (target >= 0)
 			this.ingredients.get(target).setQuantity(i.getQuantity());
-		else
-			notifyUser(2);
 	}
 
 	/* removes a specified ingredient
 	 * 
 	 */
-	public void removeIngredient(FullItem i) {
+	public void removeIngredient(IngredientItem i) {
 		int target = this.getIngredientIndex(i);
 		
 		// check if the item is found
 		if (target >= 0)
 			this.ingredients.remove(target);
-		else
-			notifyUser(2);
 	}
 
 	/* converts a specified ingredient into a specified one
 	 * does not modify quantity, nor does it test quantity
 	 */
-	public void changeIngredient(FullItem i) {
+	public void changeIngredient(IngredientItem i) {
 		int target = this.getIngredientIndex(i);
 		
 		// check if the item is found
 		if (target >= 0)
 			this.ingredients.get(target).setName(i.getName());
-		else
-			notifyUser(2);
 	}
 }
